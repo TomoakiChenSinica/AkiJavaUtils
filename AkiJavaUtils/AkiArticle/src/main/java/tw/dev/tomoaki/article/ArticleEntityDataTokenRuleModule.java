2e6b6f6a996af8c6e9f5481130203140189e37fb
TomoakiChenSinica <tomoaki.sinica@gmail.com>
2021-07-09 16:42:26 +0800

    Commit AkiArticle

---
+++ AkiJavaUtils/AkiArticle/src/main/java/tw/dev/tomoaki/article/ArticleEntityDataTokenRuleModule.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tw.dev.tomoaki.article;
+
+/**
+ *
+ * @author tomoaki
+ */
+public interface ArticleEntityDataTokenRuleModule<T> {
+    
+    public void addRule(ArticleCreator creator, T data);
+
+}
