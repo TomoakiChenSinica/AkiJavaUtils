31a80e03aefb5e0b7eb5d15f94a3951603dcecfa
TomoakiChenSinica <tomoaki.sinica@gmail.com>
2021-07-19 16:28:30 +0800

    新增AkiFramework，一些方便延伸使用的基本框架

---
+++ AkiJavaUtils/AkiFramework/AkiDataMiner/src/main/java/tw/dev/tomoaki/util/dataminer/AbstractDataMiner.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tw.dev.tomoaki.util.dataminer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Tomoaki Chen
+ */
+public abstract class AbstractDataMiner<T, Q extends DataMinerOptionQuery> {
+    
+    private List<T>oriDataList;
+    protected Q preQuery;
+    private Boolean forceSearch = false;
+    
+    public List<T> doMining(Q query) {
+        List<T> dataList = null;
+        if (!forceSearch && justNeedFilter(query)) {                       
+            dataList = this.doFilter(query);
+        } else {
+            this.oriDataList = this.doSearch(query); //其實doSearch裡面就有存
+            dataList = this.doFilter(query);
+            this.forceSearch = false;
+        }
+        preQuery = this.copyQuery(query);
+        return dataList;
+    }
+    
+    public void turnOnForceSearch() {
+        this.forceSearch = true;
+    }
+    
+    /**
+     * public -> protected 
+     */    
+    private List<T> doFilter(Q query) {
+        List<T> resultList = new ArrayList(oriDataList);
+        resultList = this.tryFilter(oriDataList, query);
+        return resultList;
+    }
+    
+    private List<T> doSearch(Q query) {
+        return this.trySearch(query);
+    }   
+    
+    
+    protected abstract Q copyQuery(Q query);
+    
+    protected abstract List<T> trySearch(Q query);
+    
+    protected abstract List<T> tryFilter(List<T> dataList, Q query);
+    
+    protected abstract Boolean justNeedFilter(Q query);
+    
+    
+        
+}
