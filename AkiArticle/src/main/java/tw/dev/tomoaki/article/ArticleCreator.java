e0e93be22e399e1db8d7fb12e8f80d94d0abbfaf
TomoakiChenSinica <tomoaki.sinica@gmail.com>
2022-02-24 15:32:58 +0800

    commit

--- AkiArticle/src/main/java/tw/dev/tomoaki/article/ArticleCreator.java
+++ AkiArticle/src/main/java/tw/dev/tomoaki/article/ArticleCreator.java
@@ -5,6 +5,7 @@
  */
 package tw.dev.tomoaki.article;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -17,109 +18,92 @@ import java.util.Set;
  * @author tomoaki
  */
 public abstract class ArticleCreator {
-    
+
     private Map<String, String> tokensReplaceMapper;
-    
-    
-    
+
     public final static String TOKEN_OLD_NOW_YEAAR = "${NowYear}";
-    public final static String TOKEN_OLD_ROC_YEAR = "${rocYear}"; //protected -> public 
-    
+    public final static String TOKEN_OLD_ROC_YEAR = "${rocYear}";
+
     public final static String TOKEN_NOW_YEAR = "${Common.NowYear}";
-    public final static String TOKEN_NOW_ROC_YEAR = "${Common.NowRocYear}"; 
-    
-     
-    public ArticleCreator(){
+    public final static String TOKEN_NOW_ROC_YEAR = "${Common.NowRocYear}";
+
+    public ArticleCreator() {
         this.doVariableInit();
         this.doBaseReplaceRulesSetup();
     }
-    
-    public ArticleCreator(Boolean useBaseMapper){
+
+    public ArticleCreator(Boolean useBaseMapper) {
         this.doVariableInit();
-        if(useBaseMapper == true){
+        if (useBaseMapper == true) {
             this.doBaseReplaceRulesSetup();
         }
     }
-    
-    protected void doVariableInit(){
-        this.tokensReplaceMapper = new LinkedHashMap();
-    }
-    
-    protected void doBaseReplaceRulesSetup(){
-//        this.addTokenReplaceRule(TOKEN_OLD_NOW_YEAAR, DateTimeProvider.GetYear());     
-//        this.addTokenReplaceRule(TOKEN_OLD_ROC_YEAR, DateTimeProvider.GetYear() - 1911);        
-//        this.addTokenReplaceRule(TOKEN_NOW_YEAR, DateTimeProvider.GetYear());
-//        this.addTokenReplaceRule(TOKEN_NOW_ROC_YEAR, DateTimeProvider.GetYear() - 1911);
-    }
-    
+
     protected abstract void doCustomRulesSetup();    
-   
-    private String replaceTokens(String oriText, Map<String, String> tokensMapper){
-        String newText = oriText;
-        Set<Map.Entry<String, String>> tokensMapperEntrySet = tokensMapper.entrySet();
-        for(Map.Entry<String, String> tokensMapperEntry : tokensMapperEntrySet){
-            String token = tokensMapperEntry.getKey();
-            String word = tokensMapperEntry.getValue();
-            if(token != null && word != null){
-                newText = newText.replace(token, word);
-            }
-        }                
-        return newText;
-    }
     
-    
-   
-    
-    public void addTokenReplaceRule(String token, String word){
+    public void addTokenReplaceRule(String token, String word) {
         this.tokensReplaceMapper.put(token, word);
     }
-    
-    public void addTokenReplaceRule(String token, Integer num){
+
+    public void addTokenReplaceRule(String token, Integer num) {
         String strNum = Integer.toString(num);
         this.tokensReplaceMapper.put(token, strNum);
     }
-    
-    public void addTokenReplaceRule(String token, Long num){
+
+    public void addTokenReplaceRule(String token, Long num) {
         String strNum = Long.toString(num);
         this.tokensReplaceMapper.put(token, strNum);
     }
-    
-//    public void addTokenReplaceRule(String token, java.util.Date date){
-//        String strDate = DateTimeProvider.DateToString(date);
-//        this.tokensReplaceMapper.put(token, strDate);
-//    }
-    
-//    public void addTokenReplaceRule(String token, java.sql.Date date){
-//        String strDate = DateTimeProvider.DateToString(date);
-//        this.tokensReplaceMapper.put(token, strDate);
-//    }
-    
-    public void addTokenReplaceRules(Map<String, String> tokenReplaceMapper){
+
+    public void addTokenReplaceRules(Map<String, String> tokenReplaceMapper) {
         Set<Map.Entry<String, String>> tokensMapperEntrySet = tokenReplaceMapper.entrySet();
-        for(Map.Entry<String, String> tokensMapperEntry : tokensMapperEntrySet){
+        for (Map.Entry<String, String> tokensMapperEntry : tokensMapperEntrySet) {
             String token = tokensMapperEntry.getKey();
             String word = tokensMapperEntry.getValue();
             this.tokensReplaceMapper.put(token, word);
-        }            
+        }
     }
-    
-    public String getArticle(String oriText){        
-        this.doCustomRulesSetup();        
+
+    public String getArticle(String oriText) {
+        this.doCustomRulesSetup();
         String newText = this.replaceTokens(oriText, tokensReplaceMapper);
         return newText;
     }
-    
-    public List<String> getTokensList(){
+
+    public List<String> getTokensList() {
         List<String> tokensList = new ArrayList();
         tokensList.addAll(this.tokensReplaceMapper.keySet());
         return tokensList;
     }
-    
-    
 
-    public Map<String, String> getTokenMap(){
+    public Map<String, String> getTokenMap() {
         return this.tokensReplaceMapper;
     }
-    
-            
+
+    private void doVariableInit() {
+        this.tokensReplaceMapper = new LinkedHashMap();
+    }
+
+    private void doBaseReplaceRulesSetup() {
+        LocalDate today = LocalDate.now();
+        Integer thisYear = today.getYear();
+        Integer taiwanThisYear = thisYear - 1911;
+        this.addTokenReplaceRule(TOKEN_OLD_NOW_YEAAR, thisYear);
+        this.addTokenReplaceRule(TOKEN_OLD_ROC_YEAR, taiwanThisYear);
+        this.addTokenReplaceRule(TOKEN_NOW_YEAR, thisYear);
+        this.addTokenReplaceRule(TOKEN_NOW_ROC_YEAR, taiwanThisYear);
+    }
+
+    private String replaceTokens(String oriText, Map<String, String> tokensMapper) {
+        String newText = oriText;
+        Set<Map.Entry<String, String>> tokensMapperEntrySet = tokensMapper.entrySet();
+        for (Map.Entry<String, String> tokensMapperEntry : tokensMapperEntrySet) {
+            String token = tokensMapperEntry.getKey();
+            String word = tokensMapperEntry.getValue();
+            if (token != null && word != null) {
+                newText = newText.replace(token, word);
+            }
+        }
+        return newText;
+    }
 }
