c48d2cc772a5b21f4327280e8d842b9766a38bb3
TomoakiChenSinica <tomoaki.sinica@gmail.com>
2022-03-29 09:17:57 +0800

    ArticleToken 開始進入可以用的階段

--- AkiArticle/src/main/java/tw/dev/tomoaki/article/helper/ArticleHelper.java
+++ AkiArticle/src/main/java/tw/dev/tomoaki/article/helper/ArticleHelper.java
@@ -7,9 +7,10 @@ package tw.dev.tomoaki.article.helper;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
-import tw.dev.tomoaki.article.annotaion.ArticleToken;
 
 /**
  *
@@ -23,18 +24,27 @@ public class ArticleHelper {
         return fields;
     }
     
-    public static List<String> obtainModuleTokenList(Object module) throws IllegalArgumentException, IllegalAccessException {
+    public static List<String> obtainModuleTokenList(Object module) throws IllegalArgumentException, IllegalAccessException, InvocationTargetException {
         List<String> tokenList = new ArrayList();
         Field[] fields = ArticleHelper.obtainFields(module);
         for(Field field : fields) {            
-            String attrName = field.getName();
+            String tokenName = field.getName();
+            System.out.println("tokenName= " + tokenName);
 //            if(attrName.startsWith("TOKEN_")) {
 //                String attrValue = (String)field.get(module);
 //                tokenList.add(attrValue);                                
 //            }
             Annotation[] annotations =field.getDeclaredAnnotations();
             for(Annotation annotation : annotations) {
-                System.out.println(annotation);
+                System.out.println(annotation);                      
+                Method[] methods = annotation.annotationType().getDeclaredMethods();
+                for(Method method : methods) {
+                    if(method.getParameterTypes().length == 0 && method.getReturnType() != void.class) {
+                        String attrName = method.getName();
+                        Object attrValue = method.invoke(annotation);
+                        System.out.println("attrName= " + attrName + ", attrValue= " + attrValue);
+                    }
+                }
             }
 //            Annotation tokenAnnotation = field.getAnnotation(ArticleToken.class);            
 //            System.out.println("attrName= " + attrName + ", tokenAnnotation= " + tokenAnnotation);
