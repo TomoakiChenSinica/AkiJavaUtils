35b6e24e57d8ca067f9ac856c204d46cef0f4dff
TomoakiChenSinica <tomoaki.sinica@gmail.com>
2022-04-01 13:23:21 +0800

    新增 幾幾種 Token

--- AkiArticle/src/main/java/tw/dev/tomoaki/article/module/BasicTokenModule.java
+++ AkiArticle/src/main/java/tw/dev/tomoaki/article/module/BasicTokenModule.java
@@ -26,11 +26,15 @@ public class BasicTokenModule extends ArticleIndependentTokenRuleModule {
     public final static String TOKEN_NOW_YEAR = "${Common.NowYear}";
     @ArticleToken(summary = "當下的台灣民國年份", description = "當下的台灣民國年份")
     public final static String TOKEN_NOW_ROC_YEAR = "${Common.NowRocYear}";
-    @ArticleToken(summary = "當下時間", description = "當下時間")
+    @ArticleToken(summary = "當下時間，格式為 年-月-日 時:分:秒", description = "當下時間，格式為 yyyy-MM-dd HH:mm:ss")
     public final static String TOKEN_NOW_DATE_TIME = "${Common.NowDateTime}";
+    @ArticleToken(summary = "當下時間，只到分，格式為 年-月-日 時:分", description = "當下時間，只到分，格式為 yyyy-MM-dd HH:mm")    
+    public final static String TOKEN_NOW_DATE_TIME_WITHOUT_SECOND = "${Common.NowDateTimeWithOutSecond}";
     
     
     private static final String DEFAULT_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";    
+    private static final String DEFAULT_DATE_TIME_WITHOUT_SECOND_FORMAT = "yyyy-MM-dd HH:mm";    
+    
     
     @Override
     public void addRule(ArticleCreator creator) {
@@ -44,6 +48,7 @@ public class BasicTokenModule extends ArticleIndependentTokenRuleModule {
         creator.addTokenReplaceRule(TOKEN_NOW_YEAR, thisYear);
         creator.addTokenReplaceRule(TOKEN_NOW_ROC_YEAR, taiwanThisYear);
         creator.addTokenReplaceRule(TOKEN_NOW_DATE_TIME, this.obtainDateTimeString(nowDateTime));
+        creator.addTokenReplaceRule(TOKEN_NOW_DATE_TIME_WITHOUT_SECOND, this.obtainDateTimeStringWithOutSecond(nowDateTime));        
     }
     
     private String obtainDateTimeString(LocalDateTime localDateTime) {
@@ -53,5 +58,13 @@ public class BasicTokenModule extends ArticleIndependentTokenRuleModule {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_FORMAT);
         return localDateTime.format(formatter);
     }
+    
+    private String obtainDateTimeStringWithOutSecond(LocalDateTime localDateTime) {
+        if(localDateTime == null) {
+            return null;
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_WITHOUT_SECOND_FORMAT);
+        return localDateTime.format(formatter);
+    }    
     
 }
