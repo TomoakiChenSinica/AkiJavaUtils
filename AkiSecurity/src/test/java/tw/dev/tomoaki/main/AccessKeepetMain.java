b74d129937748d94790658412e9b047406eac4d2
TomoakiChen <archerugly@gmail.com>
2022-04-09 16:04:07 +0800

    commit

--- AkiSecurity/src/test/java/tw/dev/tomoaki/main/AccessKeepetMain.java
+++ AkiSecurity/src/test/java/tw/dev/tomoaki/main/AccessKeepetMain.java
@@ -21,17 +21,6 @@ public class AccessKeepetMain {
     public static void main(String[] args) throws NoSuchAlgorithmException {
         // TODO code application logic here
 //        AccessKeeper keeper = AccessKeeper.Factory.create();
-<<<<<<< Updated upstream
-//        String password = keeper.createPassword("tomoaki.outside@gmail.com", "Netflix");
-//        System.out.println(password);
-//        System.out.println(keeper.checkPassword(password, "TomoakiTesing"));
-//        System.out.println(keeper.checkPassword(password, "TomoakiTesin"));
-        
-        for(Integer index = 1; index  <= 10 ; index++){
-            LocalDateTime nowDateTime = LocalDateTime.now();
-            System.out.println(nowDateTime);
-        }
-=======
 //        String password = keeper.createPassword("TomoakiTesing");
 //        System.out.println(keeper.checkPassword(password, "TomoakiTesing"));
 //        System.out.println(keeper.checkPassword(password, "TomoakiTesin"));
@@ -43,7 +32,6 @@ public class AccessKeepetMain {
         AccessKeeper keeper = AccessKeeper.Factory.create();
         String password = keeper.createPassword("tomoaki.outside@gmail.com", "Netflix");
         System.out.println(password);
->>>>>>> Stashed changes
     }
     
     public static void DisneyPlus() throws NoSuchAlgorithmException {
