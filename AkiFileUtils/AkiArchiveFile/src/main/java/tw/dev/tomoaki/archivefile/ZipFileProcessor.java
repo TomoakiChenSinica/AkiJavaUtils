9d7ec45298a4e9fdb46a3b6f4fb62d3cccea2eb9
TomoakiChen <archerugly@gmail.com>
2022-05-15 22:42:52 +0800

    commit

---
+++ AkiFileUtils/AkiArchiveFile/src/main/java/tw/dev/tomoaki/archivefile/ZipFileProcessor.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tw.dev.tomoaki.archivefile;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+import tw.dev.tomoaki.archivefile.entity.ArchiveFileOutputStream;
+
+/**
+ *
+ * @author arche
+ */
+public class ZipFileProcessor {
+
+//    private Integer unarchiveBufferSize = 1024 * 1024;
+    private Integer outputBufferSize = 1024 * 1024;
+    private Boolean printLog = Boolean.TRUE;
+
+    public static class Factory {
+
+        public static ZipFileProcessor create() {
+            ZipFileProcessor fileProcessor = new ZipFileProcessor();
+            return fileProcessor;
+        }
+
+        public static ZipFileProcessor create(Integer outputBufferSize) {
+            ZipFileProcessor fileProcessor = new ZipFileProcessor();
+            fileProcessor.outputBufferSize = outputBufferSize;
+            return fileProcessor;
+        }
+    }
+
+    public void doUnzip(String destDir, String filePath) throws IOException {
+        List<ArchiveFileOutputStream> afosList = ZipFileUtils.unzipArchive(destDir, filePath);
+        for (ArchiveFileOutputStream afos : afosList) {
+            FileOutputStream fos = afos.getFileOutputStream();
+            byte[] buffer = new byte[1024 * 1024];
+            fos.write(buffer);
+            fos.close();
+        }
+    }
+}
--- AkiFileUtils/AkiArchiveFile/src/test/java/tw/dev/tomoaki/main/ZipFileUtilsMain.java
+++ AkiFileUtils/AkiArchiveFile/src/test/java/tw/dev/tomoaki/main/ZipFileUtilsMain.java
@@ -8,6 +8,7 @@ package tw.dev.tomoaki.main;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
+import tw.dev.tomoaki.archivefile.ZipFileProcessor;
 import tw.dev.tomoaki.archivefile.ZipFileUtils;
 import tw.dev.tomoaki.archivefile.entity.ArchiveFileOutputStream;
 
@@ -19,13 +20,16 @@ public class ZipFileUtilsMain {
 
     public static void main(String[] args) throws IOException {
         System.out.println("ZipFileUtilsMain():");
-        String filePath = "D:\\Temp\\Temp.zip";
+        String filePath = "D:\\Temp\\AkiJavaUtils-main-mailimprove.zip";
         String destDir = "D:\\Temp\\ZipFileTest";
-        List<ArchiveFileOutputStream> afosList = ZipFileUtils.unzipArchive(destDir, filePath);
-        for (ArchiveFileOutputStream afos : afosList) {
-            FileOutputStream fos = afos.getFileOutputStream();
-            byte[] buffer = new byte[1024 * 1024];
-            fos.write(buffer);
-        }
+//        List<ArchiveFileOutputStream> afosList = ZipFileUtils.unzipArchive(destDir, filePath);
+//        for (ArchiveFileOutputStream afos : afosList) {
+//            FileOutputStream fos = afos.getFileOutputStream();
+//            byte[] buffer = new byte[1024 * 1024];
+//            fos.write(buffer);
+//            fos.close();
+//        }
+        ZipFileProcessor fileProcessor = ZipFileProcessor.Factory.create();
+        fileProcessor.doUnzip(destDir, filePath);
     }
 }
