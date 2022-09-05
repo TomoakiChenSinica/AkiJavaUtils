c09cbc7c8dc6092805332341add0510cfc6ebd20
TomoakiChenSinica <tomoaki.sinica@gmail.com>
2022-09-05 09:23:24 +0800

    commit

--- AkiEntity/src/main/java/tw/dev/tomoaki/util/entity/DataExistMap.java
+++ AkiEntity/src/main/java/tw/dev/tomoaki/util/entity/DataExistMap.java
@@ -19,85 +19,86 @@ import java.util.Map;
  * @author tomoaki
  */
 //[CHN20171221]
-public class DataExistMap<T> implements Serializable{
+public class DataExistMap<T> implements Serializable {
+
     private Map<T, Boolean> dataExistMap = new HashMap();
-    
-    
+
 //<editor-fold defaultstate="collapsed" desc="以下是constructor 相關">
-    public DataExistMap(){
+    public DataExistMap() {
     }
-        
-    public DataExistMap(DataExistMap<T> sourceDataExistMap){
+
+    public DataExistMap(DataExistMap<T> sourceDataExistMap) {
         this.dataExistMap = new HashMap(sourceDataExistMap.getDataExistMap());
-    }    
-    
-    public DataExistMap(Collection<T> dataList){
-        if(dataList!= null){
-            for(T data : dataList){
+    }
+
+    public DataExistMap(Collection<T> dataList) {
+        if (dataList != null) {
+            for (T data : dataList) {
                 this.dataExistMap.put(data, Boolean.TRUE);
             }
         }
     }
-        
-    public void useLinkedMap(){
+
+    public void useLinkedMap() {
         this.dataExistMap = new LinkedHashMap();
-    }    
+    }
 //</editor-fold>
-    
+
     public static class Factory {
-        public static <T>DataExistMap create(T... datas) {            
+
+        public static <T> DataExistMap<T> create(T... datas) {
             DataExistMap<T> dataExistMap = new DataExistMap(Arrays.asList(datas));
             return dataExistMap;
-        }
-    }    
-    
-    public void add(T data){
+        }                
+    }
+
+    public void add(T data) {
         dataExistMap.put(data, Boolean.TRUE);
     }
-    
-    public void addAll(List<T> datas){
-        if(datas != null){
-            for(T data : datas){
+
+    public void addAll(List<T> datas) {
+        if (datas != null) {
+            for (T data : datas) {
                 this.dataExistMap.put(data, Boolean.TRUE);
             }
         }
     }
-    
-    public void remove(T data){
+
+    public void remove(T data) {
         dataExistMap.remove(data);
     }
-    
-    public Boolean contains(T data){
-        if(this.dataExistMap.get(data) != null && this.dataExistMap.get(data)){
+
+    public Boolean contains(T data) {
+        if (this.dataExistMap.get(data) != null && this.dataExistMap.get(data)) {
             return true;
         } else {
             return false;
         }
     }
-    
-    public Boolean isEmpty(){
+
+    public Boolean isEmpty() {
         return this.dataExistMap.isEmpty();
     }
 
-    public List<T> getDataList(){
-        if(this.dataExistMap.keySet() != null){
+    public List<T> getDataList() {
+        if (this.dataExistMap.keySet() != null) {
             List<T> dataList = new ArrayList(this.dataExistMap.keySet());
             return dataList;
         }
         return new ArrayList();
     }
-   
+
     public Map<T, Boolean> getDataExistMap() {
         return dataExistMap;
     }
-    
-    public DataExistMap merge(DataExistMap<T> anotherDataExistMap){
-        if(anotherDataExistMap != null){
+
+    public DataExistMap merge(DataExistMap<T> anotherDataExistMap) {
+        if (anotherDataExistMap != null) {
             List<T> dataList = anotherDataExistMap.getDataList();
             //因為以上method 在list會為null的狀況會回傳 new array list ，所以不用再特地判斷這邊是否為NULL
-            for(T data : dataList){
+            for (T data : dataList) {
                 this.add(data);
-            }            
+            }
         }
         return this;
     }
@@ -108,21 +109,24 @@ public class DataExistMap<T> implements Serializable{
     }
 
     /**
-     *   notice that this will cause refrence  
+     * notice that this will cause refrence
      */
     public void setDataExistMap(Map<T, Boolean> dataExistMap) {
         this.dataExistMap = dataExistMap;
     }
-    
-    public DataExistMap<T> getCopy(){
+
+    public DataExistMap<T> getCopy() {
         DataExistMap<T> dataExistMap = new DataExistMap<T>(this);
         return dataExistMap;
     }
-    
-    
-    public List<T> existList(){
+
+    public List<T> existList() {
         List<T> existList = new ArrayList(this.dataExistMap.keySet());
         return existList;
     }
-                
+
+    public Integer size() {
+        return dataExistMap.size();
+    }
+
 }
