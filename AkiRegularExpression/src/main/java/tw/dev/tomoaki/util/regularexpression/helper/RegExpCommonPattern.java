bc9939c3a80253d6cc1dbebf19b2ba4417257774
TomoakiChenSinica <tomoaki.sinica@gmail.com>
2022-12-20 17:38:03 +0800

    Update RegExpCommonPattern.java

--- AkiRegularExpression/src/main/java/tw/dev/tomoaki/util/regularexpression/helper/RegExpCommonPattern.java
+++ AkiRegularExpression/src/main/java/tw/dev/tomoaki/util/regularexpression/helper/RegExpCommonPattern.java
@@ -19,4 +19,7 @@ public class RegExpCommonPattern {
     
     public static final String PURE_HTML_URL = "(https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*))";
     
+    
+    public static final String FILE_SUFFIX = "[^\\.]*$";
+    
 }
