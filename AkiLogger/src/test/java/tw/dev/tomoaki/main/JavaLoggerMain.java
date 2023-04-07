3cf50f54d400885bb3722996f45048e7de188396
TomoakiChen <archerugly@gmail.com>
2023-04-08 07:46:10 +0800

    真的更新 AkiLogger，之前沒進專案

---
+++ AkiLogger/src/test/java/tw/dev/tomoaki/main/JavaLoggerMain.java
@@ -0,0 +1,35 @@
+package tw.dev.tomoaki.main;
+
+
+
+import tw.dev.tomoaki.logger.LogHelper;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+
+
+/**
+ *
+ * @author arche
+ */
+public class JavaLoggerMain {
+
+    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+        test2();
+    }
+    
+    private static void test1() {
+        for(StackTraceElement stElement : Thread.currentThread().getStackTrace()) {
+            String className = stElement.getClassName();
+            System.out.println(String.format("className= %s", className));
+        }
+    }
+    
+    private static void test2() {
+        System.out.println(LogHelper.getCaller());
+    }
+}
